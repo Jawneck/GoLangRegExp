014305e1284611d3fdc1a5e8f937b4808ec9bdae
Danielis Joniskis <g00333859@gmit.ie>
2018-03-27 16:53:45 +0100

    Working on switch statement

--- nfa.go
+++ nfa.go
@@ -18,8 +18,43 @@ type nfa struct{
 }
 
 //Postfix RegExp to NFA function.
-func poregtonfa(pofix string){
+func poregtonfa(pofix string) *nfa{
+	nfaStack := []*nfa{}//This stack is an array of pointers to NFA's that is empty. A stack is LIFO.
 
+	//Loops through postfix regular expression a character/rune at a time.
+	for _, r := pxfix{
+		switch r{
+		case '.'://Pops two elements off the NFA stack.
+			frag2 := nfaStack[len(nfaStack)-1]//The last element on the stack.
+			nfaStack := nfaStack[:len(nfaStack)-1]//Every element in stack up to but not including the last element.
+			frag1 := nfaStack[len(nfaStack)-1]
+			nfaStack := nfaStack[:len(nfaStack)-1]
+
+			//Join the two fragments together
+			//Fragment 1s accept states edge1 points at fragment 2s initial state.
+			frag1.accept.edge1 = frag2.initial
+			
+			//Appends a new pointer to an nfa struct that represents new fragment(frag1 + frag2)
+			nfaStack = append(nfaStack, &nfa{initial: frag1.initial, accept: frag2.accept})
+
+		case '|'://Pops two elements off the NFA stack.
+			frag2 := nfaStack[len(nfaStack)-1]
+			nfaStack := nfaStack[:len(nfaStack)-1]
+			frag1 := nfaStack[len(nfaStack)-1]
+			nfaStack := nfaStack[:len(nfaStack)-1]
+
+			accept := state{}
+			initial := state{}
+			
+			nfaStack = append(nfaStack, &nfa{initial: frag1.initial, accept: frag2.accept})
+		case '*':
+
+		//Any character that is not one of the three special characters.
+		default:
+		}
+	}
+
+	return nfaStack[0]
 }
 
 func main(){

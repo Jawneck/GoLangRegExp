467d59e70d2db8fc3fa9651c948ddc22a16c88cb
Danielis Joniskis <g00333859@gmit.ie>
2018-03-27 13:50:52 +0100

    Added 4 regular expressions

--- regexp.go
+++ regexp.go
@@ -3,3 +3,32 @@ package main
 import (
 	"fmt"
 )
+
+//A function used to convert infix regular expressions to postfix regular expressions.
+func intopost(infix string) string{
+	postfix := ""
+	return postfix
+}
+
+func main(){
+
+	//Four regular expressions written in infix notation.
+
+	//Answer: ab.c*.
+	fmt.Println("Infix    ",  "a.b.c")
+	fmt.Println("Postfix  ",  intopost("a.b.c"))
+
+	//Answer: abd|.*
+	fmt.Println("Infix    ",  "(a.(b|d))*")
+	fmt.Println("Postfix  ",  intopost("(a.(b|d))*"))
+
+	//Answer: abd|.c*.
+	fmt.Println("Infix    ",  "a.(b|d).c*")
+	fmt.Println("Postfix  ",  intopost("a.(b|d).c*"))
+
+	//Answer: abb.+.c.
+	fmt.Println("Infix    ",  "a.(b.b)+.c")
+	fmt.Println("Postfix  ",  intopost("a.(b.b)+.c"))
+
+
+}

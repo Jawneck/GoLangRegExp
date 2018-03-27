4646f12944181372f706b5bdef367f360b2e50d0
Danielis Joniskis <g00333859@gmit.ie>
2018-03-27 16:19:46 +0100

    Added structs

--- nfa.go
+++ nfa.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+//struct called state with two possible edges that point to other states.
+type state struct{
+	symbol rune//Represents a state with one or two arrows labelled by Epsilon.
+	edge1 *state
+	edge2 *state
+}
+
+//struct which keeps track of the initial state and accept state of a fragment of the NFA.
+type nfa struct{
+	initial *state
+	accept *state
+}
+
+//Postfix RegExp to NFA function.
 func poregtonfa(pofix string){
 
 }
